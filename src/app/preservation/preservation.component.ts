3184f3e9e990c11a4bacafd74f5faf1b47d3f2eb
duraisamy97 <80744711+duraisamy97@users.noreply.github.com>
2022-07-30 19:49:29 +0530

    new

---
+++ src/app/preservation/preservation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preservation',
+  templateUrl: './preservation.component.html',
+  styleUrls: ['./preservation.component.css']
+})
+export class PreservationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
